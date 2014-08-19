34d7ab2af2ecfc305dad9b98435e683bcdd9c677
Chris Meats <cmeats@etccomputerland.com>
2014-08-19 10:21:13 -0500

    started modifiying storyboard

---
+++ InstagramRipOff/RootViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  InstagramRipOff
+//
+//  Created by Alejandro Tami on 19/08/14.
+//  Copyright (c) 2014 Alejandro Tami. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+
+
+
+@end
