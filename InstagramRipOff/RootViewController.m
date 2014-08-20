fbc97a8ef44938c917ff4dc16683ccc6a1664fc8
ale-tami <alejandrotami@gmail.com>
2014-08-20 11:36:30 -0300

    fb signup/login

--- InstagramRipOff/RootViewController.m
+++ InstagramRipOff/RootViewController.m
@@ -10,6 +10,8 @@
 
 @interface RootViewController ()
 
+@property UIActivityIndicatorView *spinner;
+
 @end
 
 @implementation RootViewController
@@ -21,13 +23,11 @@
 
 - (IBAction)onRegisterWithFacebookTapped:(id)sender
 {
-    NSArray *permissionsArray = @[ @"user_about_me", @"user_relationships", @"user_birthday", @"user_location"];
+    NSArray *permissionsArray = @[ @"user_about_me"];
     [PFFacebookUtils initializeFacebook];
     
-
     // Login PFUser using Facebook
     [PFFacebookUtils logInWithPermissions:permissionsArray block:^(PFUser *user, NSError *error) {
-      //  [_activityIndicator stopAnimating]; // Hide loading indicator
         
         if (!user) {
             if (!error) {
@@ -35,15 +35,67 @@
             } else {
                 NSLog(@"Uh oh. An error occurred: %@", error);
             }
-        } else if (user.isNew) {
-            NSLog(@"User with facebook signed up and logged in!");
-    //        [self.navigationController pushViewController:[[UserDetailsViewController alloc] initWithStyle:UITableViewStyleGrouped] animated:YES];
+//        } else if (user.isNew) {
+//            NSLog(@"User with facebook signed up and logged in!");
         } else {
+            
+            // Create request for user's Facebook data
+            [self showSpinner];
+            [self requestForFbUser: user];
             NSLog(@"User with facebook logged in!");
-    //        [self.navigationController pushViewController:[[UserDetailsViewController alloc] initWithStyle:UITableViewStyleGrouped] animated:YES];
         }
-    }];}
+        
+    }];
 
+}
+
+- (void) requestForFbUser: (PFUser *) user
+{
+     FBRequest *request = [FBRequest requestForMe];
+    
+    __block PFUser *userAux = user;
+    
+     [request startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+        if (!error) {
+            // result is a dictionary with the user's Facebook data
+            NSDictionary *userData = (NSDictionary *)result;
+            
+            NSString *facebookID = userData[@"id"];
+            NSString *name = userData[@"name"];
+            
+            NSURL *pictureURL = [NSURL URLWithString:
+                                [NSString stringWithFormat:@"https://graph.facebook.com/%@/picture?type=large&return_ssl_resources=1", facebookID]];
+            
+
+                NSURLRequest *request = [NSURLRequest requestWithURL:pictureURL];
+                
+                [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                    PFFile *imageFile = [PFFile fileWithName:@"profile.png" data:data];
+                    [userAux setObject: imageFile forKey:@"profilePicture"];
+                    [userAux setObject: name forKey:@"username"];
+                    
+                    NSError *error = nil;
+                    [userAux save:&error];
+                    
+                    NSLog(@"%@", [error description]);
+                    
+                    [self performSegueWithIdentifier:@"rootToLoginSeg" sender:self];
+                    
+                    [self.spinner removeFromSuperview];
+                    
+                 }];
+        }
+    }];
+}
+
+-(void) showSpinner{
+    
+    self.spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    self.spinner.center = CGPointMake(160, 240);
+    self.spinner.hidesWhenStopped = YES;
+    [self.view addSubview:self.spinner];
+    [self.spinner startAnimating];
+}
 
 
 @end
