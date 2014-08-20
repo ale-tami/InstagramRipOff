aba72697cbe9d56972e1964522218305271e5205
Chris Meats <cmeats@etccomputerland.com>
2014-08-20 13:48:24 -0500

    added custom collection cell

---
+++ InstagramRipOff/ExploreCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ExploreCollectionViewCell.h
+//  InstagramRipOff
+//
+//  Created by ETC ComputerLand on 8/20/14.
+//  Copyright (c) 2014 Alejandro Tami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExploreCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ InstagramRipOff/ExploreCollectionViewCell.m
@@ -0,0 +1,28 @@
+//
+//  ExploreCollectionViewCell.m
+//  InstagramRipOff
+//
+//  Created by ETC ComputerLand on 8/20/14.
+//  Copyright (c) 2014 Alejandro Tami. All rights reserved.
+//
+
+#import "ExploreCollectionViewCell.h"
+
+@implementation ExploreCollectionViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (IBAction)onFollowUserTapped:(UIButton *)sender
+{
+    
+}
+
+
+@end
