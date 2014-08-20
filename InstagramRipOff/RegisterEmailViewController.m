e16118cdb99ba381f5531b3c5672459966898131
Nicolas Semenas <nicolas@Nicolass-MacBook-Pro.local>
2014-08-19 21:20:39 -0300

    email registration finished + some storyboard modifications

--- InstagramRipOff/RegisterEmailViewController.m
+++ InstagramRipOff/RegisterEmailViewController.m
@@ -2,17 +2,19 @@
 //  RegisterEmailViewController.m
 //  InstagramRipOff
 //
-//  Created by ETC ComputerLand on 8/19/14.
-//  Copyright (c) 2014 Alejandro Tami. All rights reserved.
+//  Created by Nicolas Semenas on 8/19/14.
+//  Copyright (c) 2014 Nicolas Semenas. All rights reserved.
 //
 
 #import "RegisterEmailViewController.h"
 
-@interface RegisterEmailViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@interface RegisterEmailViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIAlertViewDelegate>
+
 @property (strong, nonatomic) IBOutlet UIImageView *userImage;
 @property (strong, nonatomic) IBOutlet UITextField *userEmailField;
 @property (strong, nonatomic) IBOutlet UITextField *usernameField;
 @property (strong, nonatomic) IBOutlet UITextField *userPasswordField;
+@property (strong, nonatomic) UIActivityIndicatorView *spinner;
 
 @end
 
@@ -21,7 +23,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    
 }
 
 - (IBAction)onAddPhotoTapped:(UIButton *)sender
@@ -32,9 +34,113 @@
     picker.sourceType =  UIImagePickerControllerSourceTypePhotoLibrary;
     
     [self presentViewController:picker animated:YES completion:NULL];
-
 }
 
+- (IBAction)onRegister:(id)sender {
+    [sender resignFirstResponder];
+    
+    if ([self checkFields]){
+        [self saveUserToParse];
+    };
+}
+
+
+
+
+#pragma mark - Helper Methods
+
+
+-(void) saveUserToParse {
+    
+    PFUser *user = [PFUser user];
+    user.username = self.usernameField.text;
+    user.password = self.userPasswordField.text;
+    user.email = self.userEmailField.text;
+    
+    NSData *imageData = UIImagePNGRepresentation(self.userImage.image);
+    PFFile *imageFile = [PFFile fileWithName:@"image.png" data:imageData];
+    [user setObject:imageFile forKey:@"profilePicture"];
+    
+    [self showSpinner];
+    
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            [self.spinner removeFromSuperview];
+            NSString *successMsg = [NSString stringWithFormat:@"Welcome user %@ !. Now try login with your username and password",self.usernameField.text];
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Success!"
+                                                            message:successMsg
+                                                           delegate:self
+                                                  cancelButtonTitle:@"Cool!"
+                                                  otherButtonTitles:nil];
+            [alert show];
+            
+        } else {
+            [self.spinner removeFromSuperview];
+            NSString *errorString = [error userInfo][@"error"];
+            NSString *errorMsg = [NSString stringWithFormat:@"%@",errorString];
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Something went wrong!"
+                                                            message:errorMsg
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"Cancel"
+                                                  otherButtonTitles:nil];
+            [alert show];
+        }
+    }];
+    
+}
+
+
+-(void) showSpinner{
+    
+    self.spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    self.spinner.center = CGPointMake(160, 240);
+    self.spinner.hidesWhenStopped = YES;
+    [self.view addSubview:self.spinner];
+    [self.spinner startAnimating];
+}
+
+
+
+-(BOOL) checkFields {
+    
+    if ([self.userEmailField.text isEqualToString:@""]) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Something went wrong!"
+                                                        message:@"Please enter a valid email address"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"Cancel"
+                                              otherButtonTitles:nil];
+        [alert show];
+        return NO;
+    }
+    
+    if ([self.usernameField.text isEqualToString:@""]) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Something went wrong!"
+                                                        message:@"Please enter a valid username"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"Cancel"
+                                              otherButtonTitles:nil];
+        [alert show];
+        return NO;
+    }
+    
+    
+    if ([self.userPasswordField.text isEqualToString:@""]) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Something went wrong!"
+                                                        message:@"Please enter a valid password"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"Cancel"
+                                              otherButtonTitles:nil];
+        [alert show];
+        return NO;
+    }
+    
+    return YES;
+    
+}
+
+
+
+#pragma mark - UIImagePicker Delegate
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
     
@@ -45,25 +151,12 @@
     
 }
 
-- (IBAction)onExitTextField:(UITextField *)sender
-{
-    [sender resignFirstResponder];
- 
-    PFUser *user = [PFUser user];
-    user.username = self.usernameField.text;
-    user.password = self.userPasswordField.text;
-    user.email = self.userEmailField.text;
-    
-    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-        if (!error) {
-            NSLog(@"Sign Up Succesfully!");
-        } else {
-            NSString *errorString = [error userInfo][@"error"];
-            NSLog(@"Error: %@",errorString);
-        }
-    }];
-    
+
+#pragma mark - UIAlertView Delegate
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    [self.navigationController popViewControllerAnimated:YES];
     
 }
 
 @end
+
